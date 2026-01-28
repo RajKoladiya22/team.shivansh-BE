368ea93b5f4f8e09efc3ca30bf62c67c7f73eeb5
koladiyaraj22 <koladiyaraj22@gmail.com>
2026-01-28 12:12:21 +0530

    socket

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,19 +1,19 @@
-// src/sockets/index.ts
+// // src/sockets/index.ts
 
-import { io } from "../server";
-import chatHandler from "./chat";
-import notificationHandler from "./notifications";
-import callHandler from "./calls";
+// import { io } from "../server";
+// import chatHandler from "./chat";
+// import notificationHandler from "./notifications";
+// import callHandler from "./calls";
 
-io.on("connection", (socket) => {
-  console.log(`Socket connected: ${socket.id}`);
+// io.on("connection", (socket) => {
+//   console.log(`Socket connected: ${socket.id}`);
 
-  // Attach handlers
-  chatHandler(socket, io);
-  notificationHandler(socket, io);
-  callHandler(socket, io);
+//   // Attach handlers
+//   chatHandler(socket, io);
+//   notificationHandler(socket, io);
+//   callHandler(socket, io);
 
-  socket.on("disconnect", () => {
-    console.log(`Socket disconnected: ${socket.id}`);
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log(`Socket disconnected: ${socket.id}`);
+//   });
+// });
