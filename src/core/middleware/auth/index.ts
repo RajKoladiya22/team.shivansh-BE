c33e455a4a527e879eb3dbc1a437eeb0759c5240
koladiyaraj22 <koladiyaraj22@gmail.com>
2026-03-03 11:25:28 +0530

    test logs top

--- src/core/middleware/auth/index.ts
+++ src/core/middleware/auth/index.ts
@@ -42,6 +42,8 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
 }
 
 export function requireRole(...allowedRoles: string[]) {
+    console.log("\n\n1. ALLOWED ROLES (RAW) →", allowedRoles);
+  console.log("\n2. ALLOWED ROLES (RAW) →", ...allowedRoles);
   const normalizedAllowed = allowedRoles.map(r => r.toUpperCase());
 
   return (req: any, res: Response, next: NextFunction) => {
